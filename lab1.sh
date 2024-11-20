e9d3b60dc85dc2acd6c944e1f980df9d0e565613
aanikandrov <username@Huawei>
2024-11-20 20:34:46 +0300

    Result commit

--- lab1.sh
+++ lab1.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Получаем текущий рабочий каталог
 current_directory=$(pwd)
 
-# Указываем имя файла, в который будем записывать путь
-output_file="current_directory.txt"
+output_file="result.txt"
 
-# Записываем путь в файл
 echo "$current_directory" > "$output_file"
 
-# Выводим сообщение о завершении
 echo "Текущий путь записан в файл $output_file"
